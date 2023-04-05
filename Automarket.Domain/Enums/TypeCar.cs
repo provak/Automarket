61a8e824b6069794b30c0e8d26f102c24eaef862
Vyacheslav Kalin <slava93omsk@mail.ru>
2023-04-05 16:27:18 +0600

    'init'

---
+++ Automarket.Domain/Enums/TypeCar.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Automarket.Domain.Enums
+{
+    public enum TypeCar
+    {
+        [Display(Name = "Легковой автомобиль")]
+        PassengerCar = 0,
+        [Display(Name = "Седан")]
+        Sedan = 1,
+        [Display(Name = "Хэтчбек")]
+        HatchBack = 2,
+        [Display(Name = "Минивэн")]
+        Minivan = 3,
+        [Display(Name = "Спортивный автомобиль")]
+        SportsCar = 4,
+        [Display(Name = "Внедорожник")]
+        Suv = 5
+    }
+}
